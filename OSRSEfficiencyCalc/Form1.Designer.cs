ea9a73f075f1d7624c2fc53ee0e7c8362dc712a0
Riley <Riley@DESKTOP-7GSNPE7>
2016-08-03 10:51:47 -0400

    Finished player lookup

--- OSRSEfficiencyCalc/Form1.Designer.cs
+++ OSRSEfficiencyCalc/Form1.Designer.cs
@@ -28,12 +28,183 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem("Attack");
+            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem("Hitpoints");
+            System.Windows.Forms.ListViewItem listViewItem3 = new System.Windows.Forms.ListViewItem("Mining");
+            System.Windows.Forms.ListViewItem listViewItem4 = new System.Windows.Forms.ListViewItem("Strength");
+            System.Windows.Forms.ListViewItem listViewItem5 = new System.Windows.Forms.ListViewItem("Agility");
+            System.Windows.Forms.ListViewItem listViewItem6 = new System.Windows.Forms.ListViewItem("Smithing");
+            System.Windows.Forms.ListViewItem listViewItem7 = new System.Windows.Forms.ListViewItem("Defence");
+            System.Windows.Forms.ListViewItem listViewItem8 = new System.Windows.Forms.ListViewItem("Herblore");
+            System.Windows.Forms.ListViewItem listViewItem9 = new System.Windows.Forms.ListViewItem("Fishing");
+            System.Windows.Forms.ListViewItem listViewItem10 = new System.Windows.Forms.ListViewItem("Ranged");
+            System.Windows.Forms.ListViewItem listViewItem11 = new System.Windows.Forms.ListViewItem("Thieving");
+            System.Windows.Forms.ListViewItem listViewItem12 = new System.Windows.Forms.ListViewItem("Cooking");
+            System.Windows.Forms.ListViewItem listViewItem13 = new System.Windows.Forms.ListViewItem("Prayer");
+            System.Windows.Forms.ListViewItem listViewItem14 = new System.Windows.Forms.ListViewItem("Crafting");
+            System.Windows.Forms.ListViewItem listViewItem15 = new System.Windows.Forms.ListViewItem("Firemaking");
+            System.Windows.Forms.ListViewItem listViewItem16 = new System.Windows.Forms.ListViewItem("Magic");
+            System.Windows.Forms.ListViewItem listViewItem17 = new System.Windows.Forms.ListViewItem("Fletching");
+            System.Windows.Forms.ListViewItem listViewItem18 = new System.Windows.Forms.ListViewItem("Woodcutting");
+            System.Windows.Forms.ListViewItem listViewItem19 = new System.Windows.Forms.ListViewItem("Runecrafting");
+            System.Windows.Forms.ListViewItem listViewItem20 = new System.Windows.Forms.ListViewItem("Slayer");
+            System.Windows.Forms.ListViewItem listViewItem21 = new System.Windows.Forms.ListViewItem("Farming");
+            System.Windows.Forms.ListViewItem listViewItem22 = new System.Windows.Forms.ListViewItem("Construction");
+            System.Windows.Forms.ListViewItem listViewItem23 = new System.Windows.Forms.ListViewItem("Hunter");
+            this.button1 = new System.Windows.Forms.Button();
+            this.playerText = new System.Windows.Forms.TextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Skill = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Level = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Experience = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Remaining = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(296, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 25);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Load Player";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // playerText
+            // 
+            this.playerText.Location = new System.Drawing.Point(166, 9);
+            this.playerText.Name = "playerText";
+            this.playerText.Size = new System.Drawing.Size(100, 20);
+            this.playerText.TabIndex = 1;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(592, 500);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.listView1);
+            this.tabPage1.Controls.Add(this.playerText);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(584, 474);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Stats";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(584, 474);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Crafting";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Skill,
+            this.Level,
+            this.Experience,
+            this.Remaining});
+            this.listView1.GridLines = true;
+            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1,
+            listViewItem2,
+            listViewItem3,
+            listViewItem4,
+            listViewItem5,
+            listViewItem6,
+            listViewItem7,
+            listViewItem8,
+            listViewItem9,
+            listViewItem10,
+            listViewItem11,
+            listViewItem12,
+            listViewItem13,
+            listViewItem14,
+            listViewItem15,
+            listViewItem16,
+            listViewItem17,
+            listViewItem18,
+            listViewItem19,
+            listViewItem20,
+            listViewItem21,
+            listViewItem22,
+            listViewItem23});
+            this.listView1.Location = new System.Drawing.Point(115, 35);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(342, 422);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // Skill
+            // 
+            this.Skill.Text = "Skill";
+            this.Skill.Width = 90;
+            // 
+            // Level
+            // 
+            this.Level.Text = "Level";
+            this.Level.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Level.Width = 40;
+            // 
+            // Experience
+            // 
+            this.Experience.Text = "Experience";
+            this.Experience.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Experience.Width = 100;
+            // 
+            // Remaining
+            // 
+            this.Remaining.Text = "Xp Remaining";
+            this.Remaining.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Remaining.Width = 100;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(616, 524);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox playerText;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader Skill;
+        private System.Windows.Forms.ColumnHeader Level;
+        private System.Windows.Forms.ColumnHeader Experience;
+        private System.Windows.Forms.ColumnHeader Remaining;
+        private System.Windows.Forms.TabPage tabPage2;
     }
 }
 
--- OSRSEfficiencyCalc/Form1.cs
+++ OSRSEfficiencyCalc/Form1.cs
@@ -3,18 +3,152 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace OSRSEfficiencyCalc
 {
+ 
     public partial class Form1 : Form
     {
+        Player curPlayer;
+        String userName;
+
+        String[,] statinfo = new String[23,4];
         public Form1()
         {
             InitializeComponent();
+            statinfo[0, 0] = "Attack";
+            statinfo[1, 0] = "Defence";
+            statinfo[2, 0] = "Strength";
+            statinfo[3, 0] = "Hitpoints";
+            statinfo[4, 0] = "Ranged";
+            statinfo[5, 0] = "Prayer";
+            statinfo[6, 0] = "Magic";
+            statinfo[7, 0] = "Cooking";
+            statinfo[8, 0] = "Woodcutting";
+            statinfo[9, 0] = "Fletching";
+            statinfo[10, 0] = "Fishing";
+            statinfo[11, 0] = "Firemaking";
+            statinfo[12, 0] = "Crafting";
+            statinfo[13, 0] = "Smithing";
+            statinfo[14, 0] = "Mining";
+            statinfo[15, 0] = "Herblore";
+            statinfo[16, 0] = "Agility";
+            statinfo[17, 0] = "Thieving"; 
+            statinfo[18, 0] = "Slayer";
+            statinfo[19, 0] = "Farming";
+            statinfo[20, 0] = "Runecrafting";
+            statinfo[21, 0] = "Hunter";
+            statinfo[22, 0] = "Construction";
+            curPlayer = new Player();
+            updateSkills();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            userName = this.playerText.Text;
+            userName = userName.Replace(' ', '_');
+            WebRequest request = WebRequest.Create("http://services.runescape.com/m=hiscore_oldschool/index_lite.ws?player=" + userName);
+            request.Credentials = CredentialCache.DefaultCredentials;
+            // Get the response.
+            try {
+                WebResponse response = request.GetResponse();
+                // Display the status.
+                Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+                // Get the stream containing content returned by the server.
+                Stream dataStream = response.GetResponseStream();
+                // Open the stream using a StreamReader for easy access.
+                StreamReader reader = new StreamReader(dataStream);
+                // Read the content.
+                string responseFromServer = reader.ReadToEnd();
+                curPlayer = new Player(userName, responseFromServer);
+                updateSkills();
+                // Clean up the streams and the response.
+                reader.Close();
+                response.Close();
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("Player lookup failed.");
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        public void updateSkills()
+        {
+            listView1.Items.Clear();
+            for (int i = 0; i < 23; i++)
+            {
+                listView1.Items.Add(new ListViewItem(new string[] { statinfo[i, 0], curPlayer.levels[i].ToString(), curPlayer.curxp[i].ToString(), curPlayer.xpremaining[i].ToString() }));
+            }
+        }
+        public void parseResponseFromHiScores(String str)
+        {
+
         }
     }
+}
+public class Player
+{
+    String name;
+    public int[] levels = new int[23];
+    public int[] curxp = new int[23];
+    public int[] xpremaining = new int[23];
+
+
+    public Player()
+    {
+        name = "";
+        for (int i = 0; i < 23; i++)
+        {
+            levels[i] = 1;
+            curxp[i] = 0;
+            xpremaining[i] = 83;
+        }
+    }
+    public Player(String name,String response)
+    {
+        name = response.Replace("_", " ");
+        char[] delimiterChars = { ',', '\n' };
+        string[] words = response.Split(delimiterChars);
+        for(int i = 0; i < 23; i++)
+        {
+            levels[i] = int.Parse(words[((i+1) * 3) + 1]);
+            curxp[i] = int.Parse(words[((i + 1) * 3) + 2]);
+            xpremaining[i] = getXpToLevel(curxp[i]);
+        }
+    }
+    public int getXpToLevel(int curXp) {
+        int lvl = 1;
+        while (Definitions.xpreqs[lvl] < curXp)
+        {
+            lvl++;
+            if(lvl == 99){
+                return 0;
+            }
+        }
+        return Definitions.xpreqs[lvl] - curXp;
+    }
+
+}
+public class Definitions
+{
+    public static int[] xpreqs = {0,83,174,276,388,512,650,801,969,1154,
+    1358,1584,1833,2107,2411,2746,3115,3523,3973,4470,5018,5624,6291,7028,
+    7842,8740,9730,10824,12031,13363,14833,16456,18247,20224,22406,24815,27473,30408,
+    33648,37224,41171,45529,50339,55649,61512,67983,75127,83014,91721,101333,
+    111945,123660,136594,150872,166636,184040,203254,224466,247886,273742,302288,
+    333804,368599,407015,449428,496254,547953,605032,668051,737627,814445,899257,992895,1096278,1210421,
+    1336443,1475581,1629200,1798808,1986068,2192818,2421087,2673114,2951373,3258594,
+    3597792,3972294,4385776,4842295,5346332,5902831,6517253,7195629,7944614,8771558,9684577,
+    10692629,11805606,13034431};
+
 }
